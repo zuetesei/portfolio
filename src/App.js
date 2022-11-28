db7ccf50edd4fefc7c9b697941d02e8f486837cd
zue <zuleikatesei@gmail.com>
2022-11-28 15:29:25 -0800

    start of react portfolio

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from './pages/Home';
+// import About from './pages/About';
+// import Portfolio from './pages/Portfolio';
+// import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+
+function App {
+    return (
+        <div className='App'>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='about' element={<About />} />
+                    <Route path='/portfolio' element={<Portfolio />} />
+                    <Route path='/contact' element={<Contact />} />
+                </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default App;
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className='toggleButton'>
+                <button></button>
+            </div>
+            <div className='links'>
+                <Link to="/"> Home </Link>
+                <Link to="/about"> About </Link>
+                <Link to="/portfolio"> Portfolio </Link>
+                <Link to="/contact"> Contact Me </Link>
+            </div>
+        </div>
+    );
+}
